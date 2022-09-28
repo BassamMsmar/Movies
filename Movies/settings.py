b63259afd14d23ae9f095e47cd2dd754c020b1e9
bassamHP <bassammsmar@gmail.com>
2022-09-28 18:56:43 +0300

    add form

--- Movies/settings.py
+++ Movies/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'Movies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviesdb',
+        'USER':'postgres',
+        'PASSWORD':'Admin',
+        'HOST':'localhost',
     }
 }
 
